703812c89925354d7cb83299be3a5fb7d5484e7e
Aric <hernandoaric@gmail.com>
2020-06-11 00:17:41 +0700

    Changed the way connecting works

--- QtTest/mainwindow.cpp
+++ QtTest/mainwindow.cpp
@@ -4,24 +4,21 @@
 #include <QMouseEvent>
 #include <QPainter>
 #include <QDebug>
-#include <iostream>
 
 QBrush redBrush(Qt::red);
 QBrush blueBrush(Qt::blue);
-QPen blackpen(Qt::black);
+QPen blackPen(Qt::black);
+QPen whitePen(Qt::gray);
 
-QBrush brush = Qt::SolidPattern;
-QColor clr = Qt::blue;
 bool adding = false;
 bool connecting = false;
 int i = 2;
-
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    blackpen.setWidth(1);
+    blackPen.setWidth(1);
     ui->setupUi(this);
     scene = new QGraphicsScene(this);
     ui->graphicsView->setScene(scene);
@@ -40,7 +37,6 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     adding = true;
-    connecting = false;
 }
 void MainWindow::on_pushButton_2_clicked()
 {
@@ -52,20 +48,19 @@ void MainWindow::mousePressEvent(QMouseEvent *ev)
 {
     ui->label->setText("X : " + QString::number(ev->x()) + ", Y : " + QString::number(ev->y()));
     if(adding){
-
-       qDebug() << scene->addEllipse(ev->x() - 25,ev->y() - 50,50,50,blackpen,redBrush);
-
+       qDebug() << scene->addEllipse(ev->x() - 25,ev->y() - 50,50,50,blackPen,redBrush);
        adding = false;
     }
     else if(connecting){
         auto* temp = qgraphicsitem_cast<QGraphicsEllipseItem *>(scene->itemAt(ev->pos(), QTransform()));
         if(temp){
-           temp->setBrush(brush);
+           temp->setBrush(blueBrush);
+           temp->setPen(whitePen);
            --i;
 //           x,y lingkaran1
 //           x,y lingkaran2
+//           Compare the two objects, if they're the same, don't establish link
         }
-
         // I'm not proud of this.
         if(i == 0){
             connecting = false;
